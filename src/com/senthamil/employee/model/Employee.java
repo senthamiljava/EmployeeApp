b76a7bda11da80e37531307c5752c163a0764b68
Revature <Revature@Revature96589>
2017-01-09 08:22:32 -0500

    Employee Model

---
+++ src/com/senthamil/employee/model/Employee.java
@@ -0,0 +1,37 @@
+package com.senthamil.employee.model;
+
+public class Employee {
+private int id;
+private String name;
+private int salary;
+public Employee(int id, String name, int salary) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.salary = salary;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getSalary() {
+	return salary;
+}
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+public int getId() {
+	return id;
+}
+@Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+}
+
+}
+
---
+++ src/com/senthamil/employee/model/TestEmployee.java
@@ -0,0 +1,11 @@
+package com.senthamil.employee.model;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Employee employee=new Employee(10,"aravind",70000);
+		System.out.println(employee);
+
+	}
+
+}
